dab0b7958506be947600041b73b160c7e6733a35
QuQ-bb <quqsue@gmail.com>
2022-07-15 16:25:35 +0900

    ++

--- src/com/iu/himart/Client.java
+++ src/com/iu/himart/Client.java
@@ -11,31 +11,15 @@ public class Client {
 		this.setPoint(10);
 	}
 	
-	public void buy(Tv tv ) {
+	public void buy(Product product) {
 		//제품을 사는 행위를 여기서 메서드만 호출하게 됐을때 해결해야한다.
 		//제품의 가격,제품의 포인트
-		this.money = this.money - tv.getPrice();
-		this.point = this.point + tv.getPoint();	
+		this.money = this.money - product.getPrice();
+		this.point = this.point + product.getPoint();	
 		System.out.println("잔액 : "+this.money);
 		System.out.println("Point : "+ this.point);
 	}
-	public void buy(Computer com) {
-		//제품을 사는 행위를 여기서 메서드만 호출하게 됐을때 해결해야한다.
-		//제품의 가격,제품의 포인트
-		this.money = this.money - com.getPrice();
-		this.point = this.point + com.getPoint();	
-		System.out.println("잔액 : "+this.money);
-		System.out.println("Point : "+ this.point);
-	}
-	public void buy(Phone phone ) {
-		//제품을 사는 행위를 여기서 메서드만 호출하게 됐을때 해결해야한다.
-		//제품의 가격,제품의 포인트
-		this.money = this.money - phone.getPrice();
-		this.point = this.point + phone.getPoint();	
-		System.out.println("잔액 : "+this.money);
-		System.out.println("Point : "+ this.point);
-	}
-	
+
 	public int getMoney() {
 		return money;
 	}
