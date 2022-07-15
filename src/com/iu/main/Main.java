6a0a67f61bcd6633380f37de59e2f8b935aa4403
QuQ-bb <quqsue@gmail.com>
2022-07-15 16:06:10 +0900

    다형성

--- src/com/iu/main/Main.java
+++ src/com/iu/main/Main.java
@@ -1,28 +1,34 @@
 package com.iu.main;
 
 import com.iu.terran.Marine;
-import com.iu.zerg.Hydra;
+import com.iu.terran.Scv;
+import com.iu.unit.Unit;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//객체 생성	
-		Marine marine = new Marine();
-		marine.setHp(30);
-		marine.setColor("");
-		
-		//메서드 호출
-		marine.info();
-		
-		Hydra hydra = new Hydra();
-		hydra.setHp(50);
-		hydra.setColor("");
-		hydra.setName("");
-		
-		//메서드 호출
-		hydra.move();
 
-	
+		Marine marine = new Marine();
+		Scv  scv = new Scv();
+		
+		//Marine is a Unit;
+		//Scv is a Unit;
+		
+		Unit unit = marine;// 말이안됨 원래 근데 자바놈이 가능하게 해둠
+				unit = scv;
+		//상속관계에서만 가능하다
+				
+		System.out.println(unit.getHp());
+		System.out.println(unit.getColor());
+		System.out.println(unit.getName());
+		//System.out.println(unit.); // 오류남 weapon은 나오지않는다 형태가 변하기 때문이다
+		//만들어져 있긴하지만 접근은 원래 unit에 있는 자기가 알고있는것까지만 접근가능하다
+		
+		//marine = unit;	//오류남 why? double을 int넣으려면 그냥 넣을 수 없다. 형변환이 필요하듯이
+		marine = (Marine)unit;	//unit을 Marine 타입으로 해서 넣어라
+		System.out.println(marine.getWeapon());
+		
+		
 		
 	}//main
 }//class
